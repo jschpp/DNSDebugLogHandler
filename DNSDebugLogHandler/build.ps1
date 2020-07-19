55ae7a455980e81dd1d6c60d01fdfc17f6d664ab
Johannes Schöpp <darkdiscover@gmail.com>
2020-07-19 17:17:32 +0200

    Build now creates a nuget package

--- DNSDebugLogHandler/build.ps1
+++ DNSDebugLogHandler/build.ps1
@@ -1,14 +1,20 @@
 ﻿# Requires -Module platyPS
 param (
     [string] $TargetPath,
-    [string] $ProjectDir
+    [string] $ProjectDir,
+    [string] $OutDir
 )
 
+$NugetSpec = Join-Path -Path $ProjectDir -ChildPath "DNSDebugLogHandler.nuspec"
 $NewModulePath = Join-Path -Path $ProjectDir -ChildPath "PowerShell"
 $LibPath = Join-Path -Path $NewModulePath -ChildPath "lib"
 $DocSrcPath = Join-Path -Path $ProjectDir -ChildPath "docs"
 $DocPath = Join-Path -Path $NewModulePath -ChildPath "en-US"
+$ModuleInfo = Test-ModuleManifest (Join-Path -Path $NewModulePath -ChildPath "DNSDebugLogHandler.psd1")
+$OutputPath = Join-Path -Path $ProjectDir -ChildPath $OutDir
 
-Copy-Item -Path $TargetPath -Destination $Destination
+Copy-Item -Path $TargetPath -Destination $LibPath
 
 New-ExternalHelp -Path $DocSrcPath -OutputPath $DocPath
+
+nuget pack $NugetSpec -Version $ModuleInfo.Version -BasePath $NewModulePath -OutputDirectory $OutputPath -exclude "*.gitkeep"
