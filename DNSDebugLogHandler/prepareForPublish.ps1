8ce604f3542691130caf466bf97d4a4b638e166a
Johannes Schöpp <darkdiscover@gmail.com>
2020-07-19 22:08:16 +0200

    add publishing

--- DNSDebugLogHandler/prepareForPublish.ps1
+++ DNSDebugLogHandler/prepareForPublish.ps1
@@ -1,4 +1,10 @@
-﻿$ModuleName = "DNSDebugLogHandler"
+﻿param (
+    [Parametermeter(Mandatory=$true)]
+    [string] $ApiKey
+)
+$ModuleName = "DNSDebugLogHandler"
 
 $NewModuleDir = New-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath $ModuleName) -ItemType Directory
 Copy-Item -Recurse -Path (Join-Path -Path $PSScriptRoot -ChildPath "Powershell\*") -Destination $NewModuleDir -Exclude ".gitkeep"
+
+Publish-Module -Path $NewModuleDir -NuGetApiKey $ApiKey
