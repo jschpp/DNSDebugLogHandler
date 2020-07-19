0e1f5d27fcf20110b266c4b6d913353ee2736e0c
Johannes Schöpp <darkdiscover@gmail.com>
2020-07-19 22:05:11 +0200

    change the way this module will be build

---
+++ DNSDebugLogHandler/prepareForPublish.ps1
@@ -0,0 +1,4 @@
+﻿$ModuleName = "DNSDebugLogHandler"
+
+$NewModuleDir = New-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath $ModuleName) -ItemType Directory
+Copy-Item -Recurse -Path (Join-Path -Path $PSScriptRoot -ChildPath "Powershell\*") -Destination $NewModuleDir -Exclude ".gitkeep"
