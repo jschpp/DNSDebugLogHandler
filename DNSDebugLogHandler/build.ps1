0a523ef298a10d1cd9d438ff810e25c94f8448f2
Johannes Schöpp <darkdiscover@gmail.com>
2020-07-19 15:28:11 +0200

    simplify build
    make it furture proof - now paths can be constructed in build.ps1 instead of passing them as parameters

--- DNSDebugLogHandler/build.ps1
+++ DNSDebugLogHandler/build.ps1
@@ -1,6 +1,8 @@
-﻿param (
+﻿Import-Module platyPS
+param (
     [string] $TargetPath,
-    [string] $Destination
+    [string] $ProjectDir
 )
+$Destination = [System.IO.Path]::Combine($ProjectDir, "PowerShell", "lib")
 
 Copy-Item -Path $TargetPath -Destination $Destination
